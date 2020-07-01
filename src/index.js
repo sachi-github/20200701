f1cc7753bb18f814c1c5cf34d54c02f02cd4e9e4
sachi-github <nakahara312@yahoo.co.jp>
2020-07-01 15:20:44 +0900

    お絵描きファイル追加

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+var buttons = 10;
+
+for (var num = buttons; num > 0; num--) {
+  var elm = document.createElement("button");
+
+  elm.innerHTML = num;
+  elm.setAttribute("id", num);
+  elm.setAttribute("class", "circle");
+
+  var function_name = "remove(" + num + ")";
+
+  elm.setAttribute("onclick", function_name);
+  document.getElementById("main").appendChild(elm);
+
+  var left_pos = 10;
+  var top_pos = 100;
+
+  left_pos = left_pos + Math.floor(Math.random() * 400);
+  top_pos = top_pos + Math.floor(Math.random() * 600);
+
+  document.getElementById(num).style.left = "" + left_pos + "px";
+  document.getElementById(num).style.top = "" + top_pos + "px";
+}
+
+var next = 1;
+
+document.remove = function(id) {
+  if (id === next) {
+    document.getElementById("main").removeChild(document.getElementById(id));
+    next = next + 1;
+  } else {
+    document.write("Game Over".fontcolor("red").fontsize(24));
+  }
+
+  if (next > buttons) {
+    document.write("Game Clear!".fontcolor("red").fontsize(24));
+  }
+};
